ce77ce6cbb08b9f076c4868f9fc074b3605742f2
tfxidian <tfxidian@163.com>
2018-05-22 11:25:05 +0800

    My first cpp project commit to git

---
+++ learn-cpp/cpp-01/cpp-01/cpp-01/1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "this is a c++ program" << endl;
+	int a = 4;
+	cout << "you are in level " << a << endl;
+	system("pause");
+	return 0;
+}
